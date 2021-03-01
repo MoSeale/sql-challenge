4647a704003de07e5ef5d0b1b4fc46440444b82b
MoSeale <57756845+MoSeale@users.noreply.github.com>
2021-02-28 23:32:37 -0500

    Queries.sql
    Complete queries for analysis

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -31,13 +31,40 @@ FROM dept_emp
 	ON departments.dept_no = dept_emp.dept_no;
 	
 	
--- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and 
+--last names begin with "B."
+SELECT employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE employees.first_name = 'Hercules' AND employees.last_name LIKE 'B%';
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name,
+-- and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+	JOIN employees 
+	ON dept_emp.emp_no = employees.emp_no
+	JOIN departments
+	ON departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales';
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, 
+--last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+	JOIN employees 
+	ON dept_emp.emp_no = employees.emp_no
+	JOIN departments
+	ON departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development';
 
 
--- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., 
+--how many employees share each last name.
 
 
--- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
-
--- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name)AS FREQUENCY
+FROM employees
+GROUP BY last_name
+ORDER BY 
+COUNT(last_name) DESC;
